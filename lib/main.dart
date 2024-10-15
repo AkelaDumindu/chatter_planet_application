0ab4cd972d68fd5bc1b4c39eb2354f33f4eb9ca8
unknown <akeladumindu2000@gmail.com>
2024-10-15 23:54:51 +0530

    routing and created register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:chatter_planet_application/firebase_options.dart';
+import 'package:chatter_planet_application/router/router.dart';
+import 'package:chatter_planet_application/utilz/colors.dart';
 import 'package:chatter_planet_application/views/responsive/mobile_layout.dart';
 import 'package:chatter_planet_application/views/responsive/responsive_layout.dart';
 import 'package:chatter_planet_application/views/responsive/web_layout.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,10 +21,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ResponsiveScreenLayout(
-          mobileScreenLayout: MobileScreenLayout(),
-          webScreenLayout: WebScreenLayout()),
+    return MaterialApp.router(
+      theme: ThemeData(
+          fontFamily: GoogleFonts.poppins().fontFamily,
+          brightness: Brightness.dark,
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+              backgroundColor: Colors.transparent,
+              selectedItemColor: mainOrangeColor,
+              unselectedItemColor: mainWhiteColor),
+          snackBarTheme: SnackBarThemeData(
+              backgroundColor: mainOrangeColor,
+              contentTextStyle: TextStyle(
+                color: mainWhiteColor,
+                fontSize: 16,
+              ))),
+      routerConfig: RouterClass().router,
     );
   }
 }
