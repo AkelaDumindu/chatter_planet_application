b1915b9156390c616ca44f7a45555b115757cdea
AkelaDumindu <akeladumindu2000@gmail.com>
2024-11-06 00:50:49 +0530

    created bottom navigation bar

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,16 +1,65 @@
 import 'package:chatter_planet_application/services/auth/auth_service.dart';
+import 'package:chatter_planet_application/views/main_screens/create_screen.dart';
+import 'package:chatter_planet_application/views/main_screens/feed_screen.dart';
+import 'package:chatter_planet_application/views/main_screens/profile_screen.dart';
+import 'package:chatter_planet_application/views/main_screens/reels_screen.dart';
+import 'package:chatter_planet_application/views/main_screens/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    FeedScreen(),
+    SearchScreen(),
+    CreateScreen(),
+    ReelsScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: TextButton(
-            onPressed: AuthServices().signOut, child: Text("Sign out")),
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Create',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.video_library),
+            label: 'Reels',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
